e9c9dcb475d5929bf568f5d7f657c50b3765ab71
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-05-02 10:28:28 +0800

    new

--- docs/.vuepress/components/Footer.vue
+++ docs/.vuepress/components/Footer.vue
@@ -3,6 +3,12 @@
     MIT Licensed<br>
     Copyright © 2018 <span class="blue">Allen Wong</span><br>
     Powered By <a href="https://vuepress.vuejs.org/">VuePress</a>
+    <div style="width:300px;margin:0 auto; padding:20px 0;">
+      <a target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011502000302" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+        <img src="" style="float:left;"/>
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">鄂公网安备 42011502000302号</p>
+      </a>
+    </div>
   </div>
 </template>
 <style>
