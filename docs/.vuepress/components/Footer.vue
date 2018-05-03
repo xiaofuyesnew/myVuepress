4ccde56f53ed08a8ed7f630032a9aeb744096036
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-05-03 10:13:14 +0800

    提交更新

--- docs/.vuepress/components/Footer.vue
+++ docs/.vuepress/components/Footer.vue
@@ -3,10 +3,12 @@
     MIT Licensed<br>
     Copyright © 2018 <span class="blue">Allen Wong</span><br>
     Powered By <a href="https://vuepress.vuejs.org/">VuePress</a>
-    <div style="width:300px;margin:0 auto; padding:20px 0;">
-      <a target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011502000302" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-        <img src="" style="float:left;"/>
-        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">鄂公网安备 42011502000302号</p>
+    <div class="beian" style="">
+      <div class="icp">鄂ICP备15014468号-1</div>
+      <a target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011502000302">
+        <img :src="$withBase('./police.png')" style="float:left;"/>
+        <p>鄂公网安备 42011502000302号</p>
+      </a>
       </a>
     </div>
   </div>
@@ -21,5 +23,27 @@
     text-align: center;
     color: #4e6e8e;
     line-height: 1.5rem;
+  }
+  .beian {
+    width: 300px;
+    margin: 0 auto; 
+    padding: 20px 0;
+    color: #939393;
+  }
+  .beian a {
+    display: inline-block;
+    text-decoration: none;
+    height: 20px;
+    line-height: 20px;
+    color: #939393;
+  }
+  .beian a img {
+    float: left;
+  }
+  .beian p {
+    float: left;
+    height: 20px;
+    line-height: 20px;
+    margin: 0px 0px 0px 5px;
   }
 </style>
